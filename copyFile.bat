522e9b67d21970202c0a25ddfd3f986c24bde7cd
Gabriel Messias <gmessiaspacheco@outlook.com>
2021-10-04 13:57:46 -0300

    Add files via upload

--- copyFile.bat
+++ copyFile.bat
@@ -1,3 +1,3 @@
 chcp 65001
 set backupFilename=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
-echo f | xcopy "origin\path\file.format" "destiny\path" /y /h /r /e /c
+echo f | xcopy "origin\path\file.format" "destiny\path\name%backupFilename%.format" /y /h /r /e /c
